32a32e639fb4e98648ff3861d8af280397cf8507
SpeedyEquip <maxbeau19@gmail.com>
2026-02-17 12:05:33 -0700

    Update tailwind.config.js

--- speedy-equipment/tailwind.config.js
+++ speedy-equipment/tailwind.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./app/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
